9c2a6e7df2ab56de03166d0c5f128e98e03bf3c5
jiangqinhua <jiangqinhua1993@gmail.com>
2020-03-20 21:06:07 +0800

    Update 5.4.js

--- 5.4.js
+++ 5.4.js
@@ -75,3 +75,104 @@ const eval_dispatch = list(
     go_to(label("unknown_expression_type"))
 );
 
+// Evaluating simple expressions
+connst eval_self = list(
+"ev_self_eval",
+    assign("val", reg("exp")),
+    go_to(reg("continue")));
+
+connst eval_var = list(
+"ev_variable",
+    assign("val", op("lookup_variable_value"), reg("exp"), reg("env")),
+    go_to(reg("continue")));
+
+connst eval_quoted = list(
+"ev_quoted",
+    assign("val", op("text_of_quotation"), reg("exp")),
+    go_to(reg("continue")));
+
+connst eval_lambda = list(
+"ev_lambda",
+    assign("unev", op("lambda_parameters"), reg("exp")),
+    assign("exp", op("lambda_body"), reg("exp")),
+    assign("val", op("make_procedure"), reg("unev"), reg("exp"), reg("env")),
+    go_to(reg("continue")));
+
+// Evaluating function applications
+connst eval_application = list(
+"connst ev_application",
+    save("continue"),
+    save("env"),
+    assign("unev", op("operands"), reg("exp")),
+    save("unev"),
+    assign("exp", op("operator"), reg("exp")),
+    assign("continue", label("ev_appl_did_operator")),
+    go_to(label("eval_dispatch")));
+
+
+connst eval_appl_operator = list(
+"ev_appl_did_operator",
+    restore("unev"),                  // the operands
+    restore("env"),
+    assign("argl", op("empty_arglist")),
+    assign("fun", reg("val")),       // the operator
+    test(op("has_no_operands"), reg("unev")),
+    branch(label("apply_dispatch")),
+    save("fun"));
+
+connst eval_operand_loop = list(
+"ev_appl_operand_loop",
+    save("argl"),
+    assign("exp", (op("first_operand"), reg("unev"))),
+    test(op("is_last_operand"), reg("unev")),
+    branch(label("ev_appl_last_arg")),
+    save("env"),
+    save("unev"),
+    assign("continue", (label("ev_appl_accumulate_arg"))),
+    go_to(label("eval_dispatch")));
+
+connst eval_appl_accumlate_arg = list(
+"ev_appl_accumulate_arg",
+    restore("unev"),
+    restore("env"),
+    restore("argl"),
+    assign("argl", op("adjoin_arg"), reg("val"), reg("argl")),
+    assign("unev", op("rest_operands"), reg("unev")),
+    go_to(label("ev_appl_operand_loop")));
+
+
+connst eval_appl_last_arg = list(
+"ev_appl_last_arg",
+      assign("continue", label("ev_appl_accum_last_arg")),
+      go_to(label("eval_dispatch")));
+
+// Function application
+connst eval_appl_accum_last_arg = list(
+"ev_appl_accum_last_arg",
+      restore(argl),
+      assign("argl", op("adjoin_arg"), reg("val"), reg("argl")),
+      restore("fun"),
+      go_to(label("apply_dispatch")));
+
+connst apply_dispatch = list(
+"apply_dispatch",
+    test(op("is_primitive_procedure"), reg("fun")),
+    branch(label("primitive_apply")),
+    test(op("is_compound_procedure"), reg("fun")),
+    branch(label("compound_apply")),
+    go_to(label("unknown_procedure_type")));
+
+
+connst primitive_apply = list(
+"primitive_apply",
+    assign("val", op("apply_primitive_procedure"), reg("fun"), reg("argl")),
+    restore("continue"),
+    go_to(reg("continue")));
+
+connst compoud_apply = list(
+"compound_apply",
+    assign("unev", op("procedure_parameters"), reg("fun")),
+    assign("env", op("procedure_environment") reg("fun")),
+    assign("env", op("extend_environment"), reg("unev"), reg("argl"), reg("env")),
+    assign("unev", op("procedure_body"), reg("fun")),
+    go_to(label("ev_sequence")));
